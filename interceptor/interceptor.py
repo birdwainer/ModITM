0800277e75a6422dbdfb849db766d816298f765c
Ryan <rashley@iqt.org>
2023-10-30 14:28:57 -0400

    server component working as desired

--- interceptor/interceptor.py
+++ interceptor/interceptor.py
@@ -5,16 +5,13 @@ import httpx
 import io
 
 
-@route('/axis-cgi/jpg/image.cgi')
+@route('/image')
 def moditm():
     #Sample URI
     #http://192.168.1.222/axis-cgi/jpg/image.cgi?resolution=1920x1080&compression=5&camera=1
-    camera_ip = "192.168.1.222"
-    path = "/axis-cgi/jpg/image.cgi"
-    resolution = request.query.resolution
-    compression = request.query.compression
-    camera_id = request.query.camera
-    forwarding_uri = f"http://{camera_ip}{path}?resolution={resolution}&compression={compression}&camera={camera_id}"
+    server_ip = "192.168.1.222"
+    path = "/image"
+    forwarding_uri = f"http://{server_ip}{path}?"
     resp = httpx.get(forwarding_uri)
     img_bytes = resp.content
     inverted = invert_img(img_bytes)
--- server/server.py
+++ server/server.py
@@ -1,21 +1,22 @@
+import glob
+import os
+import random
 from bottle import route, request, response, run
 
 @route('/image')
-def moditm():
-    #Sample URI
-    #http://192.168.1.222/axis-cgi/jpg/image.cgi?resolution=1920x1080&compression=5&camera=1
-    camera_ip = "192.168.1.222"
-    path = "/axis-cgi/jpg/image.cgi"
-    resolution = request.query.resolution
-    compression = request.query.compression
-    camera_id = request.query.camera
-    forwarding_uri = f"http://{camera_ip}{path}?resolution={resolution}&compression={compression}&camera={camera_id}"
-    resp = httpx.get(forwarding_uri)
-    img_bytes = resp.content
-    inverted = invert_img(img_bytes)
-    response.set_header('Content-type', 'image/jpeg')
-    return inverted
+def random_image():
+    base_uri='/images/VOCdevkit/VOC2012/JPEGImages/'
+    ext = '*.jpg'
+    files = glob.glob(base_uri + ext)
 
-run(host='0.0.0.0', port=5000, debug=True)
+    selected = random.choice(files)
+    img_bytes = None
+    with open(selected, 'rb') as img_file:
+        img_bytes = img_file.read()
+
+    response.set_header('Content-type', 'image/jpeg')
+    return img_bytes
+
+run(host='0.0.0.0', port=80, debug=True)
 
 
