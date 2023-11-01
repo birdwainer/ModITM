ee846ff3c6c1d27329c997423a55e987459d272a
Ryan <rashley@iqt.org>
2023-11-01 14:40:23 -0400

    stupid mac vs

--- attacker/startup.sh
+++ attacker/startup.sh
@@ -2,6 +2,11 @@
 
 sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 --source 192.168.1.221 --destination 192.168.1.222  -j DNAT --to-destination 192.168.1.64
+
+if [ ! -d "/var/log/nginx" ]; then
+  mkdir /var/log/nginx
+fi
+
 nginx -c /etc/nginx/nginx.conf &
 
 tail -f /dev/null
