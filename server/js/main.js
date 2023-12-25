9f6e205b0e2aed9e9b077e0933d9523a8d6f57e7
Ari Chadda <59665721+arichadda@users.noreply.github.com>
2023-12-25 14:46:23 -0500

    reCAPTCHA-like UI

---
+++ server/js/main.js
@@ -0,0 +1,115 @@
+const cellOptions = [
+  {
+    classification: "car",
+    imgSrc:
+      "https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2372&q=80",
+  },
+  {
+    classification: "car",
+    imgSrc:
+      "https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+  },
+  {
+    classification: "motorcycle",
+    imgSrc:
+      "https://images.unsplash.com/photo-1558981806-ec527fa84c39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+  },
+  {
+    classification: "bus",
+    imgSrc:
+      "https://images.unsplash.com/photo-1562620669-98104534c6cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+  },
+  {
+    classification: "car",
+    imgSrc:
+      "https://images.unsplash.com/photo-1549317661-bd32c8ce0db2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+  },
+  {
+    classification: "bridge",
+    imgSrc:
+      "https://images.unsplash.com/photo-1638567860967-4d5b82e9e126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2662&q=80",
+  },
+  {
+    classification: "stoplight",
+    imgSrc:
+      "https://images.unsplash.com/photo-1472070153210-15e27d938957?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+  },
+  {
+    classification: "boat",
+    imgSrc:
+      "https://images.unsplash.com/photo-1605281317010-fe5ffe798166?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2288&q=80",
+  },
+  {
+    classification: "stopsign",
+    imgSrc:
+      "https://images.unsplash.com/photo-1618737739013-aed8938604fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+  },
+];
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
+
+let shuffledOptions = [];
+let humanPercent = 10;
+
+function createCaptcha() {
+  shuffledOptions = shuffle(cellOptions);
+
+  const captcha = document.querySelector("div.captcha");
+
+  let innerHTML = "";
+  for (var option of shuffledOptions) {
+    innerHTML += `
+    <div class="captcha-cell" data-classification="${option.classification}">
+      <img src="${option.imgSrc}"  />
+    </div>
+    `;
+  }
+
+  captcha.innerHTML = innerHTML;
+
+  Array.from(document.querySelectorAll(".captcha-cell")).forEach((el) => {
+    el.addEventListener("click", () => {
+      el.classList.toggle("selected");
+    });
+  });
+}
+document.addEventListener("DOMContentLoaded", function(){
+  createCaptcha();
+});
+
+
+const submitButton = document.querySelector(".btn-submit");
+submitButton.addEventListener("click", () => {
+  const numberOfCars = shuffledOptions.filter(
+    (opt) => opt.classification == "car"
+  ).length;
+  const selectedElements = Array.from(
+    document.querySelectorAll(".captcha-cell.selected")
+  );
+
+  const countMismatch = numberOfCars != selectedElements.length;
+  const invalidSelections = selectedElements.filter(
+    (opt) => opt.dataset.classification != "car"
+  );
+
+  if (invalidSelections.length || countMismatch) {
+    console.log("error");
+    document.body.classList.add("error");
+  } else {
+    console.log("succeed");
+    document.body.classList.remove("error");
+    createCaptcha();
+  }
+});
--- server/server.py
+++ server/server.py
@@ -1,22 +1,41 @@
 import glob
 import os
 import random
-from bottle import route, request, response, run
+from bottle import route, request, response, run, static_file
 
-@route('/image')
+
+@route("/image")
 def random_image():
-    base_uri='/images/VOCdevkit/VOC2012/JPEGImages/'
-    ext = '*.jpg'
+    base_uri = "/images/VOCdevkit/VOC2012/JPEGImages/"
+    ext = "*.jpg"
     files = glob.glob(base_uri + ext)
 
     selected = random.choice(files)
     img_bytes = None
-    with open(selected, 'rb') as img_file:
+    with open(selected, "rb") as img_file:
         img_bytes = img_file.read()
 
-    response.set_header('Content-type', 'image/jpeg')
+    response.set_header("Content-type", "image/jpeg")
     return img_bytes
 
-run(host='0.0.0.0', port=80, debug=True)
+
+@route("/<filename:re:.*\.js>")
+def send_js(filename):
+    return static_file(filename, root="./js/")
 
 
+@route("/<filename:re:.*\.css>")
+def send_css(filename):
+    return static_file(filename, root="./css/")
+
+
+@route("/")
+def index():
+    return open("./html/index.html").read()
+
+@route("/<filename:re:.*\.(ico)>")
+def send_favicon(filename):
+    return static_file(filename, root="./images/")
+# @route("/<filename:re:.*\.(jpg|png|ico)>")
+
+run(host="0.0.0.0", port=80, debug=True)
