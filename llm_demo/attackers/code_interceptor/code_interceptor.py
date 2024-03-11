2383d17a9c3571b8e9a8caa2cab75ab1906458c8
Ryan <rashley@iqt.org>
2024-03-11 12:40:01 -0400

    it works, just without the proxy

--- llm_demo/attackers/code_interceptor/code_interceptor.py
+++ llm_demo/attackers/code_interceptor/code_interceptor.py
@@ -21,8 +21,9 @@ def fetch_good_response(route, data):
     server_ip = os.environ.get('ORIGINAL_OLLAMA_HOST')
     port = os.environ.get('ORIGINAL_OLLAMA_PORT')
     uri = f"http://{server_ip}:{port}/api/{route}"
+    headers = {'Content-Type': 'application/json'}
     output = ""
-    with httpx.stream("POST", uri, json=data, headers={"Content-Type": "application/json"}) as resp:
+    with httpx.stream("POST", uri, json=data, headers=headers) as resp:
         for txt in resp.iter_text():
             res = json.loads(txt)
             output += res['response']
@@ -33,7 +34,8 @@ def stream_response(route, data_to_forward, original_model_name):
     server_ip = os.environ.get('OLLAMA_HOST')
     port = os.environ.get('OLLAMA_PORT')
     forwarding_uri = f"http://{server_ip}:{port}/api/{route}"
-    with httpx.stream("POST", forwarding_uri, json=data_to_forward, headers={"Content-Type": "application/json"}) as resp:
+    headers = {'Content-Type': 'application/json'}
+    with httpx.stream("POST", forwarding_uri, json=data_to_forward, headers=headers, timeout=180.0) as resp:
         for txt in resp.iter_text():
             res = json.loads(txt)
             res['model'] = original_model_name
@@ -45,18 +47,20 @@ def stream_response(route, data_to_forward, original_model_name):
 def moditm_chat():
     request_data = request.json
     original_model_name = request_data['model']
-    good_response = fetch_good_response("generate", request_data)
+    # good_response = fetch_good_response("generate", request_data)
     rewritten_data = rewrite_data(request_data, "dolphin-mistral:latest")
-    response.set_header("Content-Type","application/x-ndjson")
-    return stream_response("chat", rewritten_data, original_model_name)
+    # response.set_header("Content-Type","application/x-ndjson")
+    # return stream_response("chat", rewritten_data, original_model_name)
+    return HTTPResponse(stream_response("chat", rewritten_data, original_model_name), status=200, headers={'Content-type': 'application/x-ndjson'})
 
 @route("/api/generate", method='POST')
 def moditm_generate():
     request_data = request.json
     original_model_name = request_data['model']
-    good_response = fetch_good_response("generate", request_data)
+    # good_response = fetch_good_response("generate", request_data)
     rewritten_data = rewrite_data(request_data, "dolphin-mistral:latest")
-    response.set_header("Content-Type","application/x-ndjson")
+    # response.set_header("Content-Type","application/x-ndjson")
+    # return stream_response("generate", rewritten_data, original_model_name)
     return HTTPResponse(stream_response("generate", rewritten_data, original_model_name), status=200, headers={'Content-type': 'application/x-ndjson'})
 
 @route("/health")
@@ -64,4 +68,4 @@ def healthcheck():
     return "healthy"
 # cfg = read_config()
 
-run(host="0.0.0.0", port=11434, server='gevent', debug=True)
+run(host="0.0.0.0", port=11434, debug=True)
--- llm_demo/attackers/code_interceptor/startup.sh
+++ llm_demo/attackers/code_interceptor/startup.sh
@@ -3,4 +3,4 @@
 sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 11434 --source 192.168.2.24 --destination 192.168.2.221  -j DNAT --to-destination 192.168.2.32
 
-poetry run python code_interceptor.py
+poetry run python3 code_interceptor.py
